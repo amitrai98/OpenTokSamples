5e8cdbafeb9aeeb6f510f30239f2ace9d5250ecf
amit rai <bipulamit@gmail.com>
2016-10-03 17:44:45 +0530

    Initial commit

---
+++ 6.Simple-Multiparty/src/main/java/com/opentok/android/samples/simple_multiparty/OpenTokConfig.java
@@ -0,0 +1,14 @@
+package com.opentok.android.samples.simple_multiparty;
+
+public class OpenTokConfig {
+    // *** Fill the following variables using your own Project info from the OpenTok dashboard  ***
+    // ***                      https://dashboard.tokbox.com/projects                           ***
+
+    // Replace with your OpenTok API key
+    public static final String API_KEY = "45598312";
+    // Replace with a generated Session ID
+    public static final String SESSION_ID = "2_MX40NTU5ODMxMn5-MTQ3MTM0NTM5NDg2MH5VRnl4RC8yODBYTCt1Y3dBQ0hrVEVHY0x-fg";
+    // Replace with a generated token (from the dashboard or using an OpenTok server SDK)
+    public static final String TOKEN = "T1==cGFydG5lcl9pZD00NTU5ODMxMiZzaWc9NTlhODYyOWY4ZDNjNTlkMDVjNmZmNzEyZjRkMmI4N2QwYmM4YTdmOTpzZXNzaW9uX2lkPTJfTVg0ME5UVTVPRE14TW41LU1UUTNNVE0wTlRNNU5EZzJNSDVWUm5sNFJDOHlPREJZVEN0MVkzZEJRMGhyVkVWSFkweC1mZyZjcmVhdGVfdGltZT0xNDc1NDg5NDk0Jm5vbmNlPTAuMDgwOTE5OTY1NjM1OTg1MTQmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTQ3ODA4MTQ5NA==";
+
+}
